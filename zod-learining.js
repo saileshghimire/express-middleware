b0fbddd3d913a90d2982c364776c8f8e4084a5bd
saileshghimire <saileshghimire1@gmail.com>
2024-05-09 17:29:12 +0545

    adding zod schema in post

--- zod-learining.js
+++ zod-learining.js
@@ -8,9 +8,17 @@ app.use(express.json());
 app.post ("/health-checkup", function(req,res){
     const kidneys = req.body.kidneys;
     const response = schema.safeParse(kidneys);
-    res.send({
-        response
-    });
+    if (!response.success){
+        res.status(401).json({
+            msg:"input is not valid"
+        });
+    }
+    else{
+        res.send({
+            response
+        });
+
+    }
 });
 
 app.listen(3000);
