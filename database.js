faa7d946e2a0a4baa9aef28f7dd9e5ecd5611566
saileshghimire <saileshghimire1@gmail.com>
2024-05-12 12:47:04 +0545

    database connected

---
+++ database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect(
+    'mongodb+srv://sailesh:1234@cluster0.vm8tuzu.mongodb.net/userappnew',
+);
+
+const User = mongoose.model("users",{
+    name: String,
+    username: String,
+    password: String,
+});
+
+const user = new User({
+    name:'sailesh Ghimire',
+    email:'sailesh@gmail.com',
+    password:'123456'
+});
+user.save();
+
