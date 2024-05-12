20e03c52c963e914ff4ce3d0ea63a7bcf955eabb
saileshghimire <saileshghimire1@gmail.com>
2024-05-12 13:48:45 +0545

    post function complete

--- database.js
+++ database.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
 
 mongoose.connect(
     'mongodb+srv://sailesh:1234@cluster0.vm8tuzu.mongodb.net/userappnew',
@@ -6,14 +9,36 @@ mongoose.connect(
 
 const User = mongoose.model("users",{
     name: String,
-    username: String,
+    email: String,
     password: String,
 });
 
-const user = new User({
-    name:'sailesh Ghimire',
-    email:'sailesh@gmail.com',
-    password:'123456'
-});
-user.save();
+// const user = new User({
+//     name:'sailesh Ghimire',
+//     email:'sailesh@gmail.com',
+//     password:'123456'
+// });
+app.use(express.json());
+app.post("/signin", async function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
 
+    const exitUser = await User.findOne({email: email });
+    
+    if(exitUser){
+        return res.status(404).send("user already exit");
+    }
+
+    const user = new User({
+        name: username,
+        email: email,
+        password: password
+    });
+    user.save();
+    return res.json({
+        msg:"user is created"
+    })
+})
+
+app.listen(3000);
